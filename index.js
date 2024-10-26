5ef09e73364f8c422d636c30afd4489d5169bbad
Simon Blurton <sblurton@hotmail.co.uk>
2024-10-26 10:15:00 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,142 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.set("base", "/api/v1");
+
+const movieCompanyData = [
+  { id: "1", name: "True Film Productions" },
+  { id: "2", name: "Lazy Lemon Films" },
+  { id: "3", name: "Good old TV" },
+];
+
+const movieData = [
+  {
+    id: "1",
+    reviews: [6, 8, 7, 9, 8, 7, 8],
+    title: "A long train ride",
+    filmCompanyId: "1",
+    cost: 1020,
+    releaseYear: 2001,
+  },
+  {
+    id: "2",
+    reviews: [5, 7, 3, 4, 5, 6, 3],
+    title: "Flowers on the meadow",
+    filmCompanyId: "2",
+    cost: 983,
+    releaseYear: 1997,
+  },
+  {
+    id: "3",
+    reviews: [1, 4, 5, 2, 3, 1, 2],
+    title: "Summer",
+    filmCompanyId: "1",
+    cost: 7346,
+    releaseYear: 2015,
+  },
+  {
+    id: "4",
+    reviews: [6, 7, 4, 5, 6, 7, 3],
+    title: "Back to the garden",
+    filmCompanyId: "2",
+    cost: 364,
+    releaseYear: 2009,
+  },
+  {
+    id: "5",
+    reviews: [2, 1, 2, 1, 3, 2, 1],
+    title: "Mr John Smith",
+    filmCompanyId: "3",
+    cost: 26456,
+    releaseYear: 2021,
+  },
+];
+
+const apiV1 = express.Router();
+
+function getMovieById(id) {
+  return movieData.find((movie) => movie.id === id);
+}
+
+function getCompanyById(id) {
+  return movieCompanyData.find((company) => company.id === id);
+}
+
+function getMovieWithCompanyById(movie) {
+  const company = getCompanyById(movie.filmCompanyId);
+
+  if (!company) {
+    return { ...movie, company: { name: "Unknown" } };
+  }
+
+  return { ...movie, company };
+}
+
+apiV1.get("/movies", (_request, response) => {
+  response.header({ "access-control-allow-origin": "*" });
+  if (Math.random() < 0.8) {
+    response.status(200);
+
+    const moviesWithCompany = movieData.map((movie) =>
+      getMovieWithCompanyById(movie)
+    );
+
+    response.send(moviesWithCompany);
+  } else {
+    response.status(500);
+    response.send();
+  }
+});
+
+apiV1.put("/movies/:movieId", (request, response) => {
+  response.header({ "access-control-allow-origin": "*" });
+  response.header({ "content-type": "application/json" });
+  const movie = getMovieById(request.params.movieId);
+
+  if (!movie) {
+    response.status(404);
+    response.send({ message: "Movie not found" });
+    return;
+  }
+
+  const review = request.body.review;
+
+  if (!review) {
+    response.status(400);
+    response.send({ message: "Review is required" });
+    return;
+  }
+
+  if (
+    typeof review !== "string" ||
+    review.length === 0 ||
+    review.length > 100
+  ) {
+    response.status(400);
+    response.send({
+      message: "Review must be a string less than 100 characters",
+    });
+    return;
+  }
+
+  response.status(200);
+  response.send({
+    message: `Thank you for your review for movie ${movie.title}!`,
+  });
+});
+
+apiV1.get("/companies", (_request, response) => {
+  response.header({ "access-control-allow-origin": "*" });
+  if (Math.random() < 0.8) {
+    response.status(200);
+    response.send(movieCompanyData);
+  } else {
+    response.status(500);
+    response.send();
+  }
+});
+
+app.use(app.get("base"), apiV1);
+
+app.listen(process.env.PORT || 3000);
---
+++ readme.md
@@ -0,0 +1,42 @@
+# Optix UI tech test API
+
+I edited the API slightly as I had access to the code and believed it was not RESTful or helpful for the frontend to take advantage of client and server caching.
+
+I didn't make extensive improvements since the test is for a frontend role. I made changes to assist the frontend and future-proof the API. In a real-world scenario, I would have requested these changes be made before taking the job.
+
+I also understand that consistency is sometimes more important than changing one part to be "better" if the rest is in a different format. The code as a whole should improve and not diverge in larger projects.
+
+## Changes
+
+- Version Control - All APIs need version control.
+  - Clear Communication
+  - Testing and Stability
+  - Backward Compatibility
+  - Deprecation Management
+  - User Control
+- API Endpoint Naming Convention
+  - Use Plural Nouns (camelCase is not useful in URL endpoints)
+  - HTTP Methods for Actions (e.g., PUT to update)
+
+## Suggested Improvements
+
+- TypeScript
+- ESLint
+- File Structure - It's important to start with a structure in mind and intent. It should be flexible as the project grows and we learn more about its shape, allowing us to review it.
+- The database for reviews being an array of integers blocks any expansion and is hard to understand. My suggestion would be `{ rating: number }[]`.
+- Schemas protecting the integrity of the database and providing useful error messaging for debugging.
+
+## Installation
+
+To install the dependencies, run:
+
+```
+npm install
+```
+
+Usage
+To start the server, run:
+
+```
+npm run start
+```
